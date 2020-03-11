5ccf69320cc44f022f5951f3032dd43abec58478
Md. Ariful Islam <arifjmamun24@gmail.com>
2020-03-12 00:35:50 +0600

    Product add design added

--- src/app/pages/products/models/product.model.ts
+++ src/app/pages/products/models/product.model.ts
@@ -11,4 +11,5 @@ export interface IProduct {
   productColors: IProductColor[];
   pricing: IPricing;
   featureImageUrl: string;
+  isEnabled: boolean;
 }
--- src/app/pages/products/product-add/product-add.component.ts
+++ src/app/pages/products/product-add/product-add.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+
+import { UtilityService, AsyncService, AsyncValidationService } from '../../../shared/services';
+import { CustomVaidators } from '../../../shared/helpers/custom.validators';
+import { HeaderMenuService } from '../../../services/header-menu.service';
+import { ProductsService } from '../products.service';
 
 @Component({
-  selector: 'app-product-add',
+  selector: 'product-add',
   templateUrl: './product-add.component.html',
-  styleUrls: ['./product-add.component.css']
+  styleUrls: ['./product-add.component.scss']
 })
 export class ProductAddComponent implements OnInit {
+  productAddForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public asyncService: AsyncService,
+    private fb: FormBuilder,
+    private headerMenuService: HeaderMenuService,
+    private validationService: AsyncValidationService,
+    private utilService: UtilityService,
+    private productsService: ProductsService
+  ) {}
 
   ngOnInit() {
+    this.productAddForm = this.fb.group({
+      sku: ['', Validators.required],
+      title: [
+        '',
+        [Validators.required, Validators.minLength(2)]
+      ],
+      description: ['', Validators.required],
+      manufactureDetail: this.fb.group({
+        modelNo: ['', Validators.required],
+        releaseDate: [new Date(), Validators.required]
+      }),
+      productColors: this.fb.array([this.createProductColor()]),
+      pricing: this.fb.group({
+        price: [0, [Validators.required, Validators.min(1)]]
+      }),
+      featureImageUrl: [null, Validators.required],
+      isEnabled: [true, Validators.required]
+    });
+  }
+
+  get manufactureDetail(): FormGroup {
+    this.headerMenuService.setHeaderMenu({ title: 'Add Product', subtitle: 'Products' });
+    return this.productAddForm.get('manufactureDetail') as FormGroup;
+  }
+
+  createProductColor(): FormGroup {
+    return this.fb.group({
+      colorCode: ['', Validators.required],
+      colorName: ['', Validators.required],
+      inStock: [0, Validators.min(0)],
+      isAvailable: [true],
+      images: [null],
+      sizes: [[], Validators.required]
+    });
+  }
+
+  addMoreProductColor() {
+    this.productColors.push(this.createProductColor());
+  }
+
+  removeProductColor(index: number) {
+    this.productColors.removeAt(index);
+  }
+
+  get productColors(): FormArray {
+    return this.productAddForm.get('productColors') as FormArray;
+  }
+
+  getProductColorControl(index: number) {
+    return this.productColors.controls[index];
+  }
+
+  addProduct() {
+    // TODO: implement
   }
 
 }
