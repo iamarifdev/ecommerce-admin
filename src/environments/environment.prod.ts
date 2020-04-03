efabae42b8f161561096903bb39102869b432a84
Md. Ariful Islam <arifjmamun24@gmail.com>
2020-04-03 20:13:57 +0600

    Refactored and changed api base url to match with admin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_BASE: 'https://ewebshop-api.herokuapp.com/api',
+  API_BASE: 'https://ewebshop-api.herokuapp.com/admin/api',
   SNACKBAR_DURATION: 2000,
   DEFAULT_LANG: 'en'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_BASE: 'https://localhost:5001/api',
+  API_BASE: 'https://localhost:5001/admin/api',
   SNACKBAR_DURATION: 5000,
   DEFAULT_LANG: 'en'
 };
